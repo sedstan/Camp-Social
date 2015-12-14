a31cb44c4cbc499237767d5d0ac0bfae48b55bb9
sedstan <sedky.stanley.said@gmail.com>
2015-12-14 17:45:11 +0000

    changed the site for scrapping.

--- scrapper/app.js
+++ scrapper/app.js
@@ -5,23 +5,22 @@ var Link     = require("./models/link");
 var config   = require("./config/config");
 
 mongoose.connect(config.database);
+var q          = "uk"
+var limit      = 1000;
 
-var region  = "South+East"
-var county  = "London"
-
-var url = "http://www.ukcampsite.co.uk/sites/results.asp?region= " + region + "&county=" + county;
+var url = "https://www.coolcamping.co.uk/search?q=" + q + "&limit=1000" + limit;
 
 scrape(url);
 
 function scrape(url){
   return request.get(url, function(error, response, body){
     if (error) return (console.log(error));
-    if (response.statusCode ===200)  console.log(response)
+    if (response.statusCode ===200)  
     {
-      var $ = cheerio.load(body);
+     var $ = cheerio.load(body);
+
       $('td').each(function() {
-        var href = $(this).children("a").children("a.smallest").attr("href").match(/(?:revid=)(.*?)(?=/)
-    console.log($(this).text());
+        console.log($(this).text());
 
       });
     }
@@ -29,7 +28,7 @@ function scrape(url){
 
 }
   
-  // So we want to return body response
+  
   //then we want to sort through the table and return the children hrefs for the individual show pages for each park.
   //then we want to loop thru/refactor for each reagion and county
 
