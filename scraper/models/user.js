329b37c782ba2d423674a93b08902910d3edce60
sedstan <sedky.stanley.said@gmail.com>
2015-12-15 09:28:03 +0000

    Added bycrypt and kerberos dependencies.

--- scraper/models/user.js
+++ scraper/models/user.js
@@ -11,3 +11,13 @@ var userSchema = mongoose.Schema({
   },
   
 });
+
+userSchema.statics.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+}
+
+module.exports = mongoose.model("User", userSchema);
