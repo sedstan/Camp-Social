ed2facd72821dffa9429e818f0ee4a238ae07a63
sedstan <sedky.stanley.said@gmail.com>
2015-12-15 07:23:16 +0000

    added files for user and campsite model. Also created user schema.

---
+++ scraper/models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt');
+
+var userSchema = mongoose.Schema({
+  local: {
+    username: { type: String },
+    fullname: { type: String },
+    image: { type: String },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+  },
+  
