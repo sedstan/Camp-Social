507aaa5d7255d3a11760c0ab2630cc967ee0b21f
sedstan <sedky.stanley.said@gmail.com>
2015-12-15 17:15:42 +0000

    Updated app with dependencies.

---
+++ back-end/app.js
@@ -0,0 +1,14 @@
+var express     = require('express');
+var cors        = require('cors');
+var path        =require('path');
+var morgan      =require('morgan');
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+var passport    = require('passport');
+var cookieParser = require('cookie-parser');
+var methodOverride = require('method-override');
+var jwt         = require('jsonwebtoken');
+var app         = express();
+var config      = require('./config/config');
+var User        = require('./models/user');
+var secret      = require('./config/config').secret;
