edb0abf9aeff12fd2d0ccf3ea1f1b064df4c023d
sedstan <sedky.stanley.said@gmail.com>
2015-12-17 17:32:25 +0000

    Updated forms with styling.

--- front-end/js/controllers/homeController.js
+++ front-end/js/controllers/homeController.js
@@ -1,12 +1,5 @@
 angular
 .module('CampSocial')
-.controller('homeController', homeController);
+.controller("homeController",homeController)
 
-homeController.$inject=['User', 'TokenService', '$state', 'CurrentUser']
-function homeController (User,TokenService, $state, CurrentUser) {
-  // Screen is loaded
-//If user is not loggedin, flash error message.
-// Show Login Form(not yet written)
-
-  if(!CurrentUser)
-}
+homeController.$inject=[$state
