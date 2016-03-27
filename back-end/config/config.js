4690b3f85bf789d09ef89acac84fdfe08a3d7196
sedstan <sedky.stanley.said@gmail.com>
2016-03-27 13:11:48 +0100

    Updated the config file with best practice.

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -2,5 +2,5 @@ var databaseUrl = process.env.MONGOLAB_URI || "mongodb://localhost:27017/camping
 
 module.exports = {
   database: databaseUrl,
-  secret:  "campingisbeingonewithnature"
+  secret:  process.env.PROJECT4_SECRET
 }
